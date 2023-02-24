5907d1226453c0e6a67cd0066943aebebe40e730
Tim Afanasiev <84281349+talmkg@users.noreply.github.com>
2023-02-24 16:12:40 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://wapp-fe.vercel.app']
+    origin: 'https://wapp-fe.vercel.app'
     
   }
 })
